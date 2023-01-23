502e31add457734d84b0081cd6fa9dcabb48d204
Zeng Yuwei <zeng0070@e.ntu.edu.sg>
2023-01-23 14:32:02 +0800

    fixed usb permission, display and udev rule issue for realsense running in docker

--- dockers/ubuntu20.04_ros_noetic_realsense/docker_start.sh
+++ dockers/ubuntu20.04_ros_noetic_realsense/docker_start.sh
@@ -1,2 +1,2 @@
 docker build --build-arg PYTHON_VERSION=3.8 --build-arg ROS_DISTRO=noetic --build-arg USERNAME=$USER --build-arg UID=$UID -t ubuntu20.04:base .    
-docker run --rm --privileged  --gpus all --network host --user $USER --name ubuntu20.04 -it ubuntu20.04:base /bin/zsh
+docker run --rm --privileged -v /dev:/dev -e DISPLAY=$DISPLAY --gpus all --network host --user $USER --name ubuntu20.04 -it ubuntu20.04:base /bin/zsh
